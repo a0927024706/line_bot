3b65329a96b4d1bc30d655d3f6070fe9444f6249
Neo <a0927024706@yahoo.com.tw>
2021-04-13 01:38:53 +0800

    Birthday Card

--- app.py
+++ app.py
@@ -12,67 +12,52 @@ from linebot.models import (
 import random
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('d4RG9WGjc104ouvQ1TTczvV9Xa962ub5SabRCqI/DnnXkJOd4OOqI0I/R2Pl1C5egytXWyhBS9fV5+sK+th9+/iWaCODN5JgrZe3bBEgGP48sYcXeoLWOjziZWaoBgtI25Lr+x+2dnX+8aSkO6VHXgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'd4RG9WGjc104ouvQ1TTczvV9Xa962ub5SabRCqIDnnXkJOd4OOqI0IR2Pl1C5egytXWyhBS9fV5+sK+th9+iWaCODN5JgrZe3bBEgGP48sYcXeoLWOjziZWaoBgtI25Lr+x+2dnX+8aSkO6VHXgdB04t891Ow1cDnyilFU=')
 handler = WebhookHandler('59e11fc48798f3384e056e1086b9e18a')
 
 
-def imgur_ran():
-    while True:
-        u1 = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(5))
-        image_file = 'https://i.imgur.com/'+u1+'.jpg'
-        img = Image.open(urllib.request.urlopen(image_file))
-        width, height = img.size
-        if width != 161:
-            break
-        else:
-            continue  
-  
-    return image_file
+@app.route(callback, methods=['POST'])
+def callback()
+  # get X-Line-Signature header value
+  signature = request.headers['X-Line-Signature']
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+    app.logger.info(Request body + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    try
+      handler.handle(body, signature)
+    except InvalidSignatureError
+      print(Invalid signature. Please check your channel access tokenchannel secret.)
+       abort(400)
 
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    if '摸頭' in msg:
-        image_file = imgur_ran()
-        img = ImageSendMessage(
-            original_content_url = image_file,
-            preview_image_url = image_file
-        )
-        line_bot_api.reply_message(
-        event.reply_token, img)
+def handle_message(event)
+  msg = event.message.text
+   if '摸頭' in msg
+      img = ImageSendMessage(
+           original_content_url=httpsi.imgur.comlNj8vBV.jpg,
+           preview_image_url=httpsi.imgur.comlNj8vBV.jpg
+           )
+       line_bot_api.reply_message(
+            event.reply_token, img)
         return
+    if msg != '我要卡片'
+      r = '請輸入我要生日卡片~'
 
-    if  msg == '爸爸': 
-        r = '摸頭!好乖!'
+    if '我要生日快卡片' in msg
+      r = '我欲千里尋覓你\n喜愛如常也如是\n歡愉苦短夜色白\n你知我心為誰癡'
+    if msg = '我要真的生日卡片'
+      r = '喜歡你總是樂觀正向\n喜歡你每次見面開心的樣子\n喜歡你被我逗笑的樣子\n謝謝你喜歡我的孩子氣\n謝謝你喜歡我這個直男\n謝謝你讓我喜歡你\n我們有不少地方都不謀而合\n希望我們能在一起久久的\n麻煩你繼續包容我這個白目的男友啦~\n期待跟你一起旅行'
     line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text=r))
-    
+        event.reply_token,
+        TextSendMessage(text=r))
 
 
-
-
-        
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == __main__
+  app.run()
